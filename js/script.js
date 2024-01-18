3b503f93255e350c0e5e7542cda1984fd6c02528
skancher <sohank@gmail.com>
2024-01-17 16:05:19 -0800

    Numbers and operator buttons now work

--- js/script.js
+++ js/script.js
@@ -14,24 +14,76 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, leftNumber, rightNumber) {
+function operate(operator, a, b) {
     if (operator === "+") {
-        return add(leftNumber, rightNumber);
+        return add(a, b);
     }
     else if (operator === "-") {
-        return subtract(leftNumber, rightNumber);
+        return subtract(a, b);
     }
     else if (operator === "*") {
-        return multiply(leftNumber, rightNumber);
+        return multiply(a, b);
     }
     else if (operator === "/") {
-        return divide(leftNumber, rightNumber);
+        return divide(a, b);
     }
     else {
         return "Error";
     }
 }
 
-let leftNumber;
-let rightNumber;
-let operator;
+function typedNumber(displayText) {
+    rightNumber = displayText;
+    document.querySelector("#current-text").textContent = displayText;
+    clear = false;
+}
+
+function typedOperator(operatorPressed) {
+    operator = operatorPressed
+    leftNumber = rightNumber;
+    rightNumber = "0";
+    document.querySelector("#top-text").textContent = leftNumber + " " + operator;
+    document.querySelector("#current-text").textContent = rightNumber;
+    clear = true;
+}
+
+function typedEquals() {
+    if (operator) {
+        result = operate(operator, +leftNumber, +rightNumber);
+        document.querySelector("#top-text").textContent = leftNumber + " " + operator + " " + rightNumber;
+        document.querySelector("#current-text").textContent = result;
+        operator = "";
+        clear = true;
+    }
+}
+
+let leftNumber = "";
+let rightNumber = "0";
+let operator = "";
+let clear = true;
+
+const numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+    const number = button.textContent;
+        typedNumber(clear ? number : rightNumber + number);
+    });
+});
+
+const operatorButtons = document.querySelectorAll(".operator");
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const operatorText = button.textContent;
+        typedOperator(operatorText);
+    });
+});
+
+const equalButton = document.querySelector("#equals");
+equalButton.addEventListener("click", () => typedEquals());
+
+document.addEventListener("keydown", event => {
+    let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    if (numbers.includes(event.key)){
+        typedNumber(clear ? event.key : rightNumber + event.key);
+    }
+});
